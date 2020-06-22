f6861964d6b60f4aba56ef2bc1df707ec90f85de
snakeslayer22 <66408223+snakeslayer22@users.noreply.github.com>
2020-06-22 21:10:42 +1000

    creation on octainne and player movement

---
+++ octainne/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+
+    private Rigidbody2D rb;
+
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    public float speed;
+    private Vector2 movement;
+
+    void Start(){
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("SideSpeed", Math.Abs(movement.x));
+        animator.SetFloat("UpSpeed", movement.y);
+        if (movement.x == -1)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+}
